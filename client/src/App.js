3721af70a96d33215d488d9cd72f20536ab9bf0e
Jae Yeon <wodus2854@korea.ac.kr>
2020-12-26 16:53:07 +0900

    병합

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  function btnfunc(){
+    alert("hi")
+    console.log("아")
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         안녕하세요
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button onClick={btnfunc} >button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;

c856eca69842a2b5cf7909edbe18f1ff8d0030c2
Jae Yeon <wodus2854@korea.ac.kr>
2020-12-29 18:24:13 +0900

    logsign

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,43 +1,56 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Nav1 from '../Components/MainNav';
 import Nav2 from '../Components/SubNav';
+import { Form, Col, Container, Button, Row } from 'react-bootstrap'
+import { Link , Redirect} from 'react-router-dom'
+
 
 function Login() {
 
+
     return (
-        <Container className="d-flex justify-content-center">
+        <div>
             <Nav1 />
             <Nav2 />
-            <div className="margin-auto">
-                <Form.Label className="d-flex justify-content-center">Login</Form.Label>
+            <Container className="my-5">
+                <Row className="justify-content-center">
 
-                <Form>
-                    <Form.Group controlId="formBasicId">
-                        <Form.Row>
-                            <Form.Label for="id">User Name</Form.Label>
-                            <Col>
-                                <Form.Control type="text" id="id" className="mx-sm-3" size="sm" placeholder="User Name"></Form.Control>
-                            </Col>
-                        </Form.Row>
-                    </Form.Group>
+                    <Col md={5} xs={10} className="border">
+                        <h3 className="text-center mt-5">Login</h3>
+                        <Form className="p-5">
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Row>
-                            <Form.Label for="password">Password</Form.Label>
-                            <Col>
-                                <Form.Control type="password" id="password" className="mx-sm-3" size="sm" placeholder="Password"></Form.Control>
-                            </Col>
-                        </Form.Row>
-                    </Form.Group>
-                </Form>
 
-                <div className="loginLine">
-                    <Link to="/Signup">SignUp?</Link>
-                </div>
+                            <Form.Group controlId="formBasicId">
+                                <Form.Row>
+                                    <Form.Label for="id">User Name</Form.Label>
+                                    <Col>
+                                        <Form.Control type="text" id="id" className="mx-sm-3" size="sm" placeholder="User Name"></Form.Control>
+                                    </Col>
+                                </Form.Row>
+                            </Form.Group>
 
-                <Button variant="outline-dark" type="submit" block>Login</Button>
-            </div>
-        </Container>
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Row>
+                                    <Form.Label for="password">Password</Form.Label>
+                                    <Col>
+                                        <Form.Control type="password" id="password" className="mx-sm-3" size="sm" placeholder="Password"></Form.Control>
+                                    </Col>
+                                </Form.Row>
+                            </Form.Group>
+                            <Button variant="outline-dark" type="submit" block>Login</Button>
+                            <div className="loginLine">
+                                <Link to="/signup">SignUp?</Link>
+                            </div>
+                        </Form>
+                    </Col>
+                </Row>
+
+
+
+
+            </Container>
+        </div>
+
 
     )
 }
--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -2,86 +2,105 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Redirect } from 'react-router-dom';
 import Nav1 from '../Components/MainNav';
 import Nav2 from '../Components/SubNav';
+import { Form, Col, Container, Button, Row } from 'react-bootstrap'
 
 function Signup() {
 
 
     return (
-        <Container className="d-flex justify-content-center">
+        <div>
             <Nav1 />
             <Nav2 />
-            <div className="form-control">
-                <Form.Label className="d-flex justify-content-center">Sign Up</Form.Label>
-                <Form>
+            <Container className="my-5">
+                <Row className="justify-content-center">
+                    <Col md={6} xs={10} className="border">
+                        <h2 className="text-center mt-5">Sign Up</h2>
 
-                    <Form.Group controlId="formBasicId">
-                        <Form.Row>
-                            <Form.Label for="id">User Name</Form.Label>
-                        </Form.Row>
-                        <Col>
-                            <Form.Control type="text" id="id" size="sm" placeholder="UserName" className="mx-sm-3"></Form.Control>
-                        </Col>
-                    </Form.Group>
+                        <Form className="p-5">
 
-                    <Form.Group controlId="formBasicName">
-                        <Form.Row>
-                            <Form.Label for="name">Real Name</Form.Label>
-                        </Form.Row>
-                        <Col>
-                            <Form.Control type="text" id="name" size="sm" placeholder="Enter your Real Name" className="mx-sm-3"></Form.Control>
-                        </Col>
-                    </Form.Group>
+                            <Form.Group controlId="formBasicId">
+                                <Form.Row>
+                                    <Form.Label for="id">User Name</Form.Label>
 
-                    <Form.Group controlId="formBasicNumber">
-                        <Form.Row>
-                            <Form.Label for="number">Resident registration number</Form.Label>
-                        </Form.Row>
-                        <Col>
-                            <Form.Control type="text" id="number1" size="sm" maxlength="6" className="mx-sm-3"></Form.Control>
-                        </Col>
-                    </Form.Group>
+                                    <Col>
+                                        <Form.Control type="text" id="id" size="sm" placeholder="UserName" className="mx-sm-3"></Form.Control>
+                                    </Col>
+                                </Form.Row>
+                            </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Row>
-                            <Form.Label for="password">Password</Form.Label>
-                        </Form.Row>
-                        <Col>
-                            <Form.Control type="password" id="password" size="sm" aria-describedby="passwordHelpBlock" className="mx-sm-3"></Form.Control>
-                            <Form.Text id="password" muted>Must be 8-20 characters long.</Form.Text>
-                        </Col>
-                    </Form.Group>
+                            <Form.Group controlId="formBasicName">
+                                <Form.Row>
+                                    <Form.Label for="name">Real Name</Form.Label>
 
-                    <Form.Group controlId="formBasicPassword2">
-                        <Form.Row>
-                            <Form.Label for="password2">Confirm Password</Form.Label>
-                        </Form.Row>
-                        <Col>
-                            <Form.Control type="password" id="password2" size="sm" placeholder="Confirm Password" className="mx-sm-3"></Form.Control>
-                        </Col>
-                    </Form.Group>
+                                    <Col>
+                                        <Form.Control type="text" id="name" size="sm" placeholder="Enter your Real Name" className="mx-sm-3"></Form.Control>
+                                    </Col>
+                                </Form.Row>
+                            </Form.Group>
 
-                    <Form.Group controlId="formBasicTel">
-                        <Form.Row>
-                            <Form.Label for="tel">Phone Number</Form.Label>
-                        </Form.Row>
-                        <Col>
-                            <Form.Control type="text" id="tel" size="sm" placeholder="Enter your Phone Number" className="mx-sm-3"></Form.Control>
-                        </Col>
-                    </Form.Group>
+                            <Form.Group controlId="formBasicNumber">
+                                <Form.Row>
+                                    <Form.Label for="number">Resident registration number</Form.Label>
 
-                    <Form.Group controlId="formBasicAdd">
-                        <Form.Row>
-                            <Form.Label>address</Form.Label>
-                        </Form.Row>
-                        <Col>
-                            <Form.Control type="text" id="add" size="sm" placeholder="Enter your Address" className="mx-sm-3"></Form.Control>
-                        </Col>
-                    </Form.Group>
-                </Form>
+                                    <Col as={Row}>
+                                    <Form.Control type="text" id="number1" size="sm" maxlength="6" className="mx-sm-3" style={{width:'120px'}}></Form.Control>
+                                    -
+                                    <Form.Control type="text" id="number1" size="sm" maxlength="1" className="mx-sm-3" style={{width:'25px'}}></Form.Control>
+                                    ******
+                                    </Col>
 
-                <Button variant="outline-dark" type="submit" block>Sign Up</Button>
-            </div>
-        </Container>
+                                </Form.Row>
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Row>
+                                    <Form.Label for="password">Password</Form.Label>
+
+                                    <Col>
+                                        <Form.Control type="password" id="password" size="sm" aria-describedby="passwordHelpBlock" className="mx-sm-3"></Form.Control>
+                                        <Form.Text id="password" muted>Must be 8-20 characters long.</Form.Text>
+                                    </Col>
+                                </Form.Row>
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword2">
+                                <Form.Row>
+                                    <Form.Label for="password2">Confirm Password</Form.Label>
+
+                                    <Col>
+                                        <Form.Control type="password" id="password2" size="sm" placeholder="Confirm Password" className="mx-sm-3"></Form.Control>
+                                    </Col>
+                                </Form.Row>
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicTel">
+                                <Form.Row>
+                                    <Form.Label for="tel">Phone Number</Form.Label>
+
+                                    <Col>
+                                        <Form.Control type="text" id="tel" size="sm" placeholder="Enter your Phone Number" className="mx-sm-3"></Form.Control>
+                                    </Col>
+                                </Form.Row>
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicAdd">
+                                <Form.Row>
+                                    <Form.Label>Address</Form.Label>
+
+                                    <Col>
+                                        <Form.Control type="text" id="add" size="sm" placeholder="Enter your Address" className="mx-sm-3"></Form.Control>
+                                    </Col>
+                                </Form.Row>
+                            </Form.Group>
+
+                            <Button variant="outline-dark" type="submit" block>Sign Up</Button>
+                        </Form>
+
+                    </Col>
+                </Row>
+
+            </Container>
+        </div>
 
     )
 }

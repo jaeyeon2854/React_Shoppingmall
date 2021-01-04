9a9c2a56815a5b8bdbec2cd519cc1ef139123c21
who <loot04@naver.com>
2021-01-04 14:58:56 +0900

    1

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Signup from './Pages/Signup';
 import Product from "./Pages/Products";
+import ProductsList from './Pages/ProductsList';
 import Admin from './Pages/Admin';
 import ProductsRegist from './Pages/ProductsRegist';
 import ShoppingCart from './Pages/ShoppingCart';
@@ -19,6 +20,7 @@ ReactDOM.render(
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/list" component={ProductsList} />
         <Route path="/product"component={Product} />
         <Route path="/admin" component={Admin} />
         <Route path="/regist" component={ProductsRegist} />

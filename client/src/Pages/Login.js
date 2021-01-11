a645a94afb2d2592423e3e466e6241412529fda4
Jae Yeon <wodus2854@korea.ac.kr>
2021-01-11 16:07:55 +0900

    로그인 로그아웃 끝.

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -34,8 +34,8 @@ function Login() {
         setValidated(true);
         try {
             setError('')
-            await axios.post('/api/auth/login', user)
-            handleLogin()
+            const response=await axios.post('/api/auth/login', user)
+            handleLogin(response.data.userId)
             setSuccess(true)
         } catch (error) {
             catchErrors(error, setError)

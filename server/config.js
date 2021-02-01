922beb407b524627bdc6405cd4d1e704827ef33e
Yoon, Daeki <dowellware@gmail.com>
2021-02-01 13:33:53 +0900

    node env

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-    env: process.env.NODEENV || 'development',
+    env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3001,
     jwtSecret: process.env.JWT_SECRET || 'My_Secret_Key',
     mongoDbUri: process.env.MONGEDB_URI || 'mongodb://localhost/shopping-mall',

1f9aa29da0f6723cb29e47f5dcdbc8a6e2854de4
ksbin1025 <ksbin1025@naver.com>
2021-02-02 17:30:42 +0900

    checked

--- client/src/Components/ListCard.js
+++ client/src/Components/ListCard.js
@@ -69,7 +69,7 @@ function ListCard(props) {
                     <Card>
                         <Row className="mx-1">
                             <Col xs={2} sm={2} className="text-center my-auto">
-                                <input className="" type="checkbox" name={String(e._id)} onChange={props.checkedCart} />
+                                <input type="checkbox" name={String(e._id)} onChange={props.checkedCart} checked={e.checked} />
                             </Col>
                             <Col className="text-center">
                                 <Card.Img className="img-fluid" variant="top" src={e.productId.main_imgUrl && `/images/${e.productId.main_imgUrl}`} style={{ width: '20rem' }} />
@@ -83,7 +83,7 @@ function ListCard(props) {
                                     <Card.Text >수량</Card.Text>
                                     <div>
                                         <input type="image" name={String(e._id)} alt="마이너스" src="https://img.icons8.com/ios-glyphs/20/000000/minus-math.png" className="align-middle" onClick={props.minusNum} />
-                                        <input type="number" style={{ width: '30px' }} className="text-center align-middle mx-1" placeholder={e.count} value={e.count} readOnly></input>
+                                        <input type="number" style={{ width: '35px' }} className="text-center align-middle mx-1" placeholder={e.count} value={e.count} readOnly></input>
                                         <input type="image" name={String(e._id)} alt="플러스" src="https://img.icons8.com/ios-glyphs/20/000000/plus-math.png" className="align-middle" onClick={props.plusNum} />
                                     </div>
                                 </Card.Body>
